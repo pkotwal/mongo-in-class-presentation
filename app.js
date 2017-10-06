6a0e83c688ed6fa851d32920667ff1773b19a893
Ameya Gurjar <gurja010@umn.edu>
2017-10-06 12:50:51 -0500

    All queries done

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var models = require('./model');
+//var mongoose = require('mongoose');
+//var models = require('./model');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var Movie = mongoose.model('Movie');
+//var Movie = mongoose.model('Movie');
 
 var app = express();
+
+var db  = require('./db');
+var collection;
+
+// Connect to Mongo on start
+db.connect('mongodb://teamhasnoname:popo1234@ds161008.mlab.com:61008/csci5117test1', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  }
+    else{
+        console.log("Connected");
+        collection = db.get().collection('movies');
+        
+    }
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,27 +47,75 @@ app.use('/', index);
 app.use('/users', users);
 
 
-app.post('/mongo/insert', function(req, res){
-  var name = req.body.name;
-  var desc = req.body.desc;
-  var rating = req.body.rating;
-  var release = req.body.date;
-  var stars = req.body.stars;
-  var stars_array = stars.split(',');
-
-  var movie = new Movie();
-  movie.name = name;
-  movie.description = desc;
-  movie.rating = rating;
-  movie.release = release;
-  movie.actors = stars_array;
-
-  movie.save(function(err, movie){
-    if (err){res.status(500).send(err);}
-    res.json(movie);
-  });
-
+app.post('/mongo/insert',function(req,res){
+   var name=req.body.name,
+       desc=req.body.desc,
+       rating= parseInt(req.body.rating),
+       stars=req.body.stars,
+       date=req.body.date;
+    
+    var starray = stars.split(",");
+    for(var i = 0; i< starray.length; i++)
+        starray[i]= starray[i].trim();
+    
+    collection.insert({"name":name,"description":desc,"rating":rating,"actors":starray,"release":date}).then(function(result) {
+  console.log(result);
+        res.json(result);
 });
+});
+
+
+app.get('/mongo/findone',function(req,res){
+   var name=req.query.name;    
+    console.log(name);
+    collection.findOne({"name":name}).then(function(result) {
+  console.log(result);
+        res.json(result);
+});
+});
+
+app.get('/mongo/findall',function(req,res){
+   var rating=parseInt(req.query.rating);    
+    console.log(rating);
+
+        
+    collection.find({'rating': {$gte: rating}}).toArray(function(err,result) {
+  console.log(result);
+        res.json(result);
+});
+});
+
+app.get('/mongo/delete',function(req,res){
+   var name=req.query.name;    
+    console.log(name);
+    collection.deleteOne({name:name},function(err, result) {
+  console.log(result);
+        res.json(result);
+});
+});
+
+app.post('/mongo/updateOne',function(req,res){
+   var name=req.body.name; 
+   var desc=req.body.desc;
+    console.log(name);
+    collection.updateOne({name:name},{$set:{description:desc}},function(err, result) {
+  console.log(result);
+        res.json(result);
+});
+});
+
+app.post('/mongo/update',function(req,res){
+   var star=req.body.star; 
+   var rating=parseInt(req.body.rating);
+    console.log(star);
+    console.log(rating);
+    collection.updateMany({actors: star},{$set:{rating:rating}},function(err, result) {
+        if(err){res.send(err);}
+  console.log(result);
+        res.json(result);
+});
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
